022321aa5ef5af9961a108c8804f7db4213fe58f
Rami Alkawadri <ramialkawadri@github.com>
2023-08-18 12:40:28 +0200

    Create folders

--- install.bash
+++ install.bash
@@ -1,12 +1,26 @@
 #!/bin/bash
 #
 # Install all files
+mkdir -p ~/.config/dunst
 cp ./config/dunst/dunstrc ~/.config/dunst/dunstrc
+
+mkdir -p ~/.config/hypr
 cp ./config/hypr/* ~/.config/hypr
+
+mkdir -p ~/.config/kitty
 cp ./config/kitty/* ~/.config/kitty
+
 sudo cp ./etc/sddm.conf /etc/sddm.conf
+
+sudo mkdir -p /etc/xdg/waybar
 sudo cp ./etc/xdg/waybar/* /etc/xdg/waybar
+
+mkdir -p ~/.config/rofi
 cp -rf ./config/rofi/* ~/.config/rofi
+
+mkdir -p ~/.config/nvim
 cp -rf ./config/nvim/* ~/.config/nvim
+
+mkdir -p /usr/share/sddm/themes
 sudo cp -rf ./usr/share/sddm/themes/sugar-candy/* /usr/share/sddm/themes
 
