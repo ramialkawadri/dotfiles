88ef81dfa83157221076a9317e1e2bc7874b5fdc
Rami Alkawadri <ramialkawadri@hotmail.com>
2023-08-30 15:04:17 +0200

    Update theme, and bufferline

--- config/nvim/lua/bufferline-config.lua
+++ config/nvim/lua/bufferline-config.lua
@@ -1,14 +1,26 @@
 -- Documentation: https://github.com/akinsho/bufferline.nvim
 
-require('bufferline').setup {
+local bufferline = require('bufferline')
+
+bufferline.setup {
     options = {
         diagnostics = 'nvim_lsp',
+        style_preset = {
+            bufferline.style_preset.no_italic,
+            bufferline.style_preset.no_bold
+        },
         diagnostics_indicator = function(count, level)
             local icon = level:match('error') and ' ' or ' '
             return ' ' .. icon .. count
         end,
-        numbers = function(opts)
-            return string.format('%s', opts.raise(opts.id))
-        end
-    },
+        separator_style = 'thick',
+        offsets = {
+            {
+                filetype = "NvimTree",
+                text = "File Explorer",
+                highlight = "Directory",
+                separator = true -- use a "true" to enable the default, or set your own character
+            }
+        }
+    }
 }
--- config/nvim/lua/styling.lua
+++ config/nvim/lua/styling.lua
@@ -1,1 +1,1 @@
-vim.cmd('colorscheme github_dark_dimmed')
+vim.cmd('colorscheme hybrid')
